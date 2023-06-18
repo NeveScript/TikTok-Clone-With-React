e2e0080dc18a59d8e5ea7acc83097e726acbf826
NekoYasha <autornekoyasha@gmail.com>
2023-06-17 21:33:48 -0300

    Removed unused code block

--- src/App.js
+++ src/App.js
@@ -34,17 +34,6 @@ function App() {
             />
           );
         })}
-        {/*
-        <Video
-          likes={200}
-          comments={310}
-          shares={350}
-          username="@user2"
-          description="Cool description 02"
-          music="Epic music"
-          url="https://poqlymuephttfsljdabn.supabase.co/storage/v1/object/public/jornadadev/bird.mp4?t=2023-05-22T19%3A40%3A47.052Z"
-        />
-       */}
       </div>
     </div>
   );
